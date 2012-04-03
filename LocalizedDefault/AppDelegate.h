a537f7e139107e515aa09b4aa41a3e329cdcb364
ipodishima <marian.paul2@gmail.com>
2012-04-03 09:47:00 +0200

    Initial Commit

---
+++ LocalizedDefault/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocalizedDefault
+//
+//  Created by Marian Paul on 03/04/12.
+//  Copyright (c) 2012 iPuP SARL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LocalizedDefault/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LocalizedDefault
+//
+//  Created by Marian Paul on 03/04/12.
+//  Copyright (c) 2012 iPuP SARL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
